f2ede0f4d04e924599f5f3b65bdef5fc54dad9ca
ambdasha <your-email@example.com>
2026-03-07 22:26:32 +0300

    analizer

---
+++ internal/analizer/analizer.go
@@ -0,0 +1,38 @@
+package analyzer
+
+import (
+	"go/ast"
+
+	"github.com/ambdasha/logmsglint/internal/rules"
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "logmsglint",
+	Doc:  "checks log messages for formatting and sensitive data",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (any, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			call, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			msg, pos, ok := extractLogMessage(call)
+			if !ok {
+				return true
+			}
+
+			for _, v := range rules.ValidateMessage(msg) {
+				pass.Reportf(pos, v.Message)
+			}
+
+			return true
+		})
+	}
+
+	return nil, nil
+}
